39b37390be6d84d73796e31b67ac731f982b537e
Oleg12032002 <55206770+Oleg12032002@users.noreply.github.com>
2022-03-25 16:13:23 +0200

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -17,7 +17,7 @@ window.onload = () => {
     const box = document.querySelector('.box')
     console.log(box);
     box.addEventListener("click", ()=> {
-         document.body.innerHTML = `<div class="prize">${mas_prize[getRandomInt(mas_prize.length-1)]}</div> <div class="ttt">З Хеловіном. Бажаю веселого і захоплюючого життя без страхів, втрат, прикрощів та негараздів. Нехай чорний кіт ніколи не перебігає тобі жодної дороги, нехай кожен день твого життя сплітаються в одну велику і міцну павутину щастя</div>`
+         document.body.innerHTML = `<div class="prize">${mas_prize[getRandomInt(mas_prize.length-1)]}</div> <div class="ttt">З Хеловіном. Бажаю веселого і захоплюючого життя без страхів, втрат, прикрощів та негараздів. Нехай чорний кіт ніколи не перебігає тобі жодної дороги, нехай кожен день твого життя сплітається в одну велику і міцну павутину щастя</div>`
         document.body.innerHTML += 
             `<div class="bat bat-1"></div>`
     })
